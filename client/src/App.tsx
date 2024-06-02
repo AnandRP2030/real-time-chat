31d55b0dde4656c6eb363ef3f08cc527c3ba39f3
Anand R P <anandrp.sics@gmail.com>
2024-06-02 22:49:48 +0530

    tailwind config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,37 @@
-
 function App() {
-
   return (
     <>
       <h1> Home page</h1>
+      <button className="btn btn-neutral btn-lg">Neutral</button>
+
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
+          </div>
+        </div>
+        <div className="chat-bubble">
+          It was said that you would, destroy the Sith, not join them.
+        </div>
+      </div>
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
+          </div>
+        </div>
+        <div className="chat-bubble">
+          It was you who would bring balance to the Force
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
